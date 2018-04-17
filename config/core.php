7c2757b53358a7fd5fedb4cc34b881af4b32edb8
nunezrip <pnunez@techlaunch.io>
2018-04-16 20:05:08 -0400

    Created Core and db configs

---
+++ config/core.php
@@ -0,0 +1,9 @@
+<?php
+
+// ?Show error reporting
+error_reporting(E_ALL);
+
+// set default time zone
+date_default_timezone_set('America/New_York');
+
+
---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database {
+  // Specify your own db credentials
+  private $host = 'localhost';
+  private $db_name = 'php_react_crud';
+  private $username = 'root';
+  private $password = '';
+  public $conn; 
+
+public function geConnection() {
+  $thi->conn = null;
+
+  try {
+    $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->usrname, $this->password);
+  } catch(PDOException $exception) {
+    echo 'Connection error:' . $exception->getMessage();
+  }
+  return $this->conn;
+}
+
+}
